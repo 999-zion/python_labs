d776bc8b8475b7869cffdc05bdd08c6a0d5d08df
999-zion <zi0n999@yandex.ru>
2026-09-20 15:21:36 +0300

    01

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -11,4 +11,36 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     ans = (mi,ma)
     return ans
 
+'''
+print(min_max([3, -1, 5, 5, 0]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
 print(min_max([1.5, 2, 2.0, -3.1]))
+print(min_max([]))
+'''
+
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    nums = sorted(set(nums))
+    return nums
+
+'''
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+'''
+
+def flatten(mat: list[list | tuple]) -> list:
+    ans = []
+    for i in mat:
+        if type(i) == list or type(i) == tuple:
+            ans += i
+        else: raise TypeError("строка не строка строк матрицы")
+    return ans
+
+'''
+print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
+'''
